93c1273d019b2d5979f7ca78e63f1d326196224a
Mico Dan <micodan369@gmail.com>
2023-08-04 10:22:49 +0200

    instagram clone

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ import { name as appName } from './app.json';
 // import { Buffer } from 'buffer';
 // global.Buffer = Buffer; // very important
 AppRegistry.registerComponent(appName, () => App);
+
+
